0c12f437fe9fd7d5ed554a97d8126358c921940f
M.A.W <136363991+junincode@users.noreply.github.com>
2024-05-17 19:34:31 -0300

    Update Algoritmo Exponenciação modular.py

--- Algoritmos/Algoritmo Exponenciação modular.py
+++ Algoritmos/Algoritmo Exponenciação modular.py
@@ -1,13 +1,19 @@
+"""
+Autor: Moises Auad Werneck
+Descrição: Calcula a exponenciação modular de a^b mod n
+Entrada: 3 números inteiros
+Saída: O módulo de a^b por n
+"""
+
 def expmod(a, b, n):
     if b == 0:
         return 1
     elif b%2 == 1:
         r = a * expmod(a, b-1, n) % n
-        print("mul")
 
     elif b%2 == 0:
         r = pow(expmod(a, b/2, n), 2) % n
-        print("pow")
+    
     return r
 
-print(expmod(51, 49, 19))
+print(expmod(int(input("Digite <a> ")), int(input("Digite <b> ")) , int(input("Digite <n> "))))
